88724ba7e8deb28e1516ddbd524612bbc77f243e
Aadhithyan <aadhithyanpandian@gmail.com>
2022-08-07 22:48:32 +0800

    Removing a song from playlist feature

--- src/main/java/com/arka/spotify/controller/PlaylistController.java
+++ src/main/java/com/arka/spotify/controller/PlaylistController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -64,7 +65,16 @@ public class PlaylistController {
 		}		
 	}
 	
+	//Removing a song from playlist
+	@DeleteMapping("/playlist/{playlistId}")
+	public ResponseEntity<Song> removeFromPlaylist(@PathVariable("playlistId") long playlistId, @RequestBody Song song)
+	{
+	  Optional<Song> songData = songService.getSongById(song.getSongId());
+	  Song songObj = songData.get();
+	  
 	}
+	
+}
 
 
 
